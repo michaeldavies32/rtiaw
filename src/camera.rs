2fac7032aa81ff1b6bba9b9e23c3af7e2c32d482
michaeldavies32 <michaeldavies32@googlemail.com>
2023-09-25 20:48:14 +0100

    Moved bulk of render logic to Camera module.

--- src/camera.rs
+++ src/camera.rs
@@ -1,10 +1,120 @@
 use crate::hittable::HittableList;
+use crate::ray::Ray;
+use glam::DVec3;
+use itertools::Itertools;
+use raylib::prelude::*;
 
-struct Camera {
+// const ASPECT_RATIO: f64 = 16.0 / 9.0;
+// const IMAGE_WIDTH: u32 = 800;
+// const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32; //(IMAGE_WIDTH as f32 / ASPECT_RATIO).clamp(1.0, f32::MAX) as u32;
+//
+// const MAX_VALUE: u8 = 255;
+//
+// const VIEWPORT_HEIGHT: f64 = 2.0;
+// const VIEWPORT_WIDTH: f64 = VIEWPORT_HEIGHT * ASPECT_RATIO; //(IMAGE_WIDTH as f64 / IMAGE_HEIGHT as f64);
+//
+// const FOCAL_LENGTH: f64 = 1.0;
+// const CAMERA_CENTER: DVec3 = DVec3::ZERO;
+//
+// const VIEWPORT_U: DVec3 = DVec3::new(VIEWPORT_WIDTH, 0., 0.);
+// const VIEWPORT_V: DVec3 = DVec3::new(0., -VIEWPORT_HEIGHT, 0.);
+
+pub struct Camera {
     aspect_ratio: f64,
     image_width: u32,
+    image_height: u32,
+    max_value: u8,
+    center: DVec3,
+    pixel_delta_u: DVec3,
+    pixel_delta_v: DVec3,
+    pixel00_loc: DVec3,
+    frame: String,
 }
 
 impl Camera {
-    fn render(world: &HittableList) {}
+    pub fn new(image_width: u32, aspect_ratio: f64) -> Self {
+        let max_value = 255u8;
+        let focal_length: f64 = 1.0;
+        let image_height = (image_width as f64 / aspect_ratio) as u32;
+
+        let viewport_height: f64 = 2.0;
+        let viewport_width: f64 = viewport_height * (image_width as f64 / image_height as f64);
+
+        let viewport_u = DVec3::new(viewport_width, 0., 0.);
+        let viewport_v = DVec3::new(0., -viewport_height, 0.);
+
+        let pixel_delta_u: DVec3 = viewport_u / image_width as f64;
+        let pixel_delta_v: DVec3 = viewport_v / image_height as f64;
+
+        let center = DVec3::ZERO;
+
+        let viewport_upper_left =
+            center - DVec3::new(0., 0., focal_length) - viewport_u / 2. - viewport_v / 2.;
+
+        let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        Camera {
+            aspect_ratio,
+            image_height,
+            image_width,
+            max_value,
+            center,
+            pixel_delta_u,
+            pixel_delta_v,
+            pixel00_loc,
+            frame: String::new(),
+        }
+    }
+    pub fn render(&mut self, d: &mut RaylibDrawHandle, world: &HittableList) {
+        self.frame = (0..self.image_height)
+            .cartesian_product(0..self.image_width)
+            // .progress_count(IMAGE_HEIGHT as u64 * IMAGE_WIDTH as u64)
+            .map(|(y, x)| {
+                let pixel_center = self.pixel00_loc
+                    + (x as f64 * self.pixel_delta_u)
+                    + (y as f64 * self.pixel_delta_v);
+
+                let ray_direction = pixel_center - self.center;
+
+                let ray = Ray::new(self.center, ray_direction);
+
+                let pixel_colour = ray.colour(&world) * 255.;
+
+                d.draw_pixel(
+                    x as _,
+                    y as _,
+                    Color {
+                        r: pixel_colour.x as _,
+                        g: pixel_colour.y as _,
+                        b: pixel_colour.z as _,
+                        a: 255u8,
+                    },
+                );
+
+                format!(
+                    "{} {} {}",
+                    pixel_colour.x as u8, pixel_colour.y as u8, pixel_colour.z as u8
+                )
+            })
+            .chunks(self.image_width as usize)
+            .into_iter()
+            .map(|chunk| chunk.into_iter().join(" "))
+            .join("\n");
+    }
+
+    pub fn render_to_file(&self, path: &std::path::Path) -> std::io::Result<()> {
+        std::fs::write(
+            path,
+            format!(
+                "P3
+{} {}
+{}
+{}
+",
+                self.image_width, self.image_height, self.max_value, self.frame
+            ),
+        )?;
+
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,39 @@
-use std::ops::Range;
-
 use glam::DVec3;
-use indicatif::ProgressIterator;
-use itertools::Itertools;
-use raylib::{ffi::Color, prelude::*};
 
 mod camera;
 mod hittable;
 mod ray;
 mod shapes;
 
+use camera::Camera;
 use hittable::HittableList;
-use ray::Ray;
 use shapes::Sphere;
 
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const IMAGE_WIDTH: u32 = 800;
 const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32; //(IMAGE_WIDTH as f32 / ASPECT_RATIO).clamp(1.0, f32::MAX) as u32;
 
-const MAX_VALUE: u8 = 255;
-
-const VIEWPORT_HEIGHT: f64 = 2.0;
-const VIEWPORT_WIDTH: f64 = VIEWPORT_HEIGHT * ASPECT_RATIO; //(IMAGE_WIDTH as f64 / IMAGE_HEIGHT as f64);
-
-const FOCAL_LENGTH: f64 = 1.0;
-const CAMERA_CENTER: DVec3 = DVec3::ZERO;
-
-const VIEWPORT_U: DVec3 = DVec3::new(VIEWPORT_WIDTH, 0., 0.);
-const VIEWPORT_V: DVec3 = DVec3::new(0., -VIEWPORT_HEIGHT, 0.);
-
 fn main() -> std::io::Result<()> {
     let (mut rl, thread) = raylib::init()
         .size(IMAGE_WIDTH as _, IMAGE_HEIGHT as _)
         .title("Raytracing in a weekend")
         .build();
-    let pixel_delta_u: DVec3 = VIEWPORT_U / IMAGE_WIDTH as f64;
-    let pixel_delta_v: DVec3 = VIEWPORT_V / IMAGE_HEIGHT as f64;
-
-    let viewport_upper_left =
-        CAMERA_CENTER - DVec3::new(0., 0., FOCAL_LENGTH) - VIEWPORT_U / 2. - VIEWPORT_V / 2.;
-
-    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     let mut world = HittableList::new();
 
     world.add(Sphere::new(DVec3::new(0., 0., -1.), 0.5));
     world.add(Sphere::new(DVec3::new(0., -100.5, -1.), 100.));
 
-    let mut pixels = String::new();
+    let mut camera = Camera::new(IMAGE_WIDTH, ASPECT_RATIO);
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
-        pixels = (0..IMAGE_HEIGHT)
-            .cartesian_product(0..IMAGE_WIDTH)
-            // .progress_count(IMAGE_HEIGHT as u64 * IMAGE_WIDTH as u64)
-            .map(|(y, x)| {
-                let pixel_center =
-                    pixel00_loc + (x as f64 * pixel_delta_u) + (y as f64 * pixel_delta_v);
-                let ray_direction = pixel_center - CAMERA_CENTER;
-
-                let ray = Ray::new(CAMERA_CENTER, ray_direction);
-
-                let pixel_colour = ray.colour(&world) * 255.;
-
-                d.draw_pixel(
-                    x as _,
-                    y as _,
-                    Color {
-                        r: pixel_colour.x as _,
-                        g: pixel_colour.y as _,
-                        b: pixel_colour.z as _,
-                        a: 255u8,
-                    },
-                );
-
-                format!(
-                    "{} {} {}",
-                    pixel_colour.x as u8, pixel_colour.y as u8, pixel_colour.z as u8
-                )
-            })
-            .chunks(IMAGE_WIDTH as usize)
-            .into_iter()
-            .map(|chunk| chunk.into_iter().join(" "))
-            .join("\n");
+        camera.render(&mut d, &world);
     }
 
-    std::fs::write(
-        "output.ppm",
-        format!(
-            "P3
-{IMAGE_WIDTH} {IMAGE_HEIGHT}
-{MAX_VALUE}
-{pixels}
-"
-        ),
-    )?;
-
+    camera.render_to_file(std::path::Path::new("output.ppm"))?;
     world.clear();
 
     Ok(())
-}
-
-fn hit_sphere(center: &DVec3, radius: f64, ray: &Ray) -> Option<f64> {
-    let oc = ray.origin - *center;
-    // let a = ray.direction.dot(ray.direction);
-    let a = ray.direction.length_squared();
-    // let b = 2.0 * oc.dot(ray.direction);
-    let half_b = oc.dot(ray.direction);
-    // let c = oc.dot(oc) - radius.powi(2);
-    let c = oc.length_squared() - radius.powi(2);
-    // let discriminant = b.powi(2) - 4. * a * c;
-    let discriminant = half_b.powi(2) - a * c;
-
-    if discriminant < 0. {
-        None
-    } else {
-        // Some((-b - discriminant.sqrt()) / (2.0 * a))
-        Some((-half_b - discriminant.sqrt()) / a)
-    }
 }
